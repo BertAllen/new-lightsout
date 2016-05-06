7abd14092dc07239ebe2ab6e0b629657d125d07a
Bert Allen <mrlaughingdragon@aol.com>
2016-05-06 11:38:24 -0600

    working!!

--- app/controller/maincontroller.js
+++ app/controller/maincontroller.js
@@ -6,26 +6,35 @@ angular.module('LightsOut', []).controller('MainController', function () {
 
 
     mc.makegrid = function (g) {
-        for (var x = g-1; x > -1; x--) {
+        // debugger;
+        for (var x = 0; x < g; x++) {
             mc.grid[x] = [];
             // var colum = i
-            for (var y = 0; y <= x; y++) {
+            
+            for (var y = (g - x - 1); y >= 0; y--) {
                 // var row = j
                 mc.grid[x][y] = { row: x, col: y, lit: false, neighbors: [] };
 
             }
         }
-        for (var a = g-1; a > -1; a--) {
-            for (var b = 0; b <= a; b++){
+        for (var a = 0; a < mc.grid.length; a++) {
+            for (var b = 0; b < mc.grid[a].length; b++){
                 for (var c = -1; c < 2; c += 2){
                     if (a + c >= 0 && a + c < g) {
-                        mc.grid[a][b].neighbors.push(mc.grid[a+c][b])
+                        if (mc.grid[a + c][b]) {
+                            mc.grid[a][b].neighbors.push(mc.grid[a + c][b])
+                        }    
                     }
                     if (b + c >= 0 && b + c < g) {
-                        mc.grid[a][b].neighbors.push(mc.grid[a][b+c])
+                        if (mc.grid[a][b + c]) {
+                            mc.grid[a][b].neighbors.push(mc.grid[a][b + c])
+                        }    
                     }
-                    if (a + c >= 0 && a + c < g && b + c >= 0 && b + c < g) {
-                        mc.grid[a][b].neighbors.push(mc.grid[a+c][b+c])
+                    // debugger;
+                    if (a + c >= 0 && a + c < g && b + (c * -1) >= 0 && b + (c * -1) < g) {
+                        if (mc.grid[a + c][b + (c * -1)]) {
+                            mc.grid[a][b].neighbors.push(mc.grid[a + c][b + (c * -1)])
+                        }    
                     }
                 }
             }

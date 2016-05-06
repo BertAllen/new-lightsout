b375fca16e8c301afda814b14c8f36a321dbdc80
Bert Allen <mrlaughingdragon@aol.com>
2016-05-06 12:16:17 -0600

    should be perfect

--- app/controller/maincontroller.js
+++ app/controller/maincontroller.js
@@ -1,40 +1,35 @@
 angular.module('LightsOut', []).controller('MainController', function () {
     var mc = this;
-    // $scope.test = "angular works";
-    mc.grid = [];
-
-
-
     mc.makegrid = function (g) {
-        // debugger;
+        if (g < 0) {
+            g = g * -1;
+        } else if (g = 0) {
+            return;
+        }
+        mc.grid = [];
         for (var x = 0; x < g; x++) {
             mc.grid[x] = [];
-            // var colum = i
-            
             for (var y = (g - x - 1); y >= 0; y--) {
-                // var row = j
                 mc.grid[x][y] = { row: x, col: y, lit: false, neighbors: [] };
-
             }
         }
         for (var a = 0; a < mc.grid.length; a++) {
-            for (var b = 0; b < mc.grid[a].length; b++){
-                for (var c = -1; c < 2; c += 2){
+            for (var b = 0; b < mc.grid[a].length; b++) {
+                for (var c = -1; c < 2; c += 2) {
                     if (a + c >= 0 && a + c < g) {
                         if (mc.grid[a + c][b]) {
                             mc.grid[a][b].neighbors.push(mc.grid[a + c][b])
-                        }    
+                        }
                     }
                     if (b + c >= 0 && b + c < g) {
                         if (mc.grid[a][b + c]) {
                             mc.grid[a][b].neighbors.push(mc.grid[a][b + c])
-                        }    
+                        }
                     }
-                    // debugger;
                     if (a + c >= 0 && a + c < g && b + (c * -1) >= 0 && b + (c * -1) < g) {
                         if (mc.grid[a + c][b + (c * -1)]) {
                             mc.grid[a][b].neighbors.push(mc.grid[a + c][b + (c * -1)])
-                        }    
+                        }
                     }
                 }
             }
@@ -45,6 +40,5 @@ angular.module('LightsOut', []).controller('MainController', function () {
         cell.neighbors.forEach(function (nab) {
             nab.lit = !nab.lit;
         })
-}
-    
+    }
 });
